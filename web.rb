8b71f69e43c35372b1545ac7cd0bcf7812e4a994
Adam Pash <adam.pash@gmail.com>
2015-03-16 12:33:04 -0400

    Updated

--- web.rb
+++ web.rb
@@ -13,25 +13,25 @@ client = Kinja::Client.new(
   user: ENV["KINJA_USER"],
   password: ENV["KINJA_PASSWORD"]
 )
+
 twitter = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV["CONSUMER_KEY"]
   config.consumer_secret     = ENV["CONSUMER_SECRET"]
   config.access_token        = ENV["ACCESS_TOKEN"]
   config.access_token_secret = ENV["ACCESS_SECRET"]
 end
+
 markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(autolink: true, hard_wrap: true))
 
 post '/' do
   email = EmailReceiver.receive request
   subject = EmailReceiver.scrub_fwd(email.subject)
-  puts email.body.decoded
   post = client.post(
     headline: "Subject: #{subject}",
     body: markdown.render(SimpleScrubber.scrub(email.body.decoded, [:email, :phone])),
     status: "DRAFT"
   )
   url = post["data"]["permalink"]
-  puts post
   puts url
   twitter.update "#{subject} #{url}"
   status 200
