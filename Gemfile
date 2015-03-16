9973b0b36faa9f7b44f6ed91426b7d5c60d1adab
Adam Pash <adam.pash@gmail.com>
2015-03-16 12:02:42 -0400

    Added twitter stuff

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'puma'
 gem 'incoming'
 gem 'redcarpet'
 gem 'simple_scrubber'
+gem 'twitter'
+gem 'dotenv'
 
 group :development do
   gem 'rspec'
--- web.rb
+++ web.rb
@@ -3,6 +3,8 @@ require 'kinja'
 require 'json'
 require 'redcarpet'
 require 'simple_scrubber'
+# require 'dotenv'
+# Dotenv.load
 require_relative './lib/post_client'
 require_relative './lib/email_receiver'
 
@@ -10,15 +12,25 @@ client = Kinja::Client.new(
   user: ENV["KINJA_USER"],
   password: ENV["KINJA_PASSWORD"]
 )
+twitter = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["CONSUMER_KEY"]
+  config.consumer_secret     = ENV["CONSUMER_SECRET"]
+  config.access_token        = ENV["ACCESS_TOKEN"]
+  config.access_token_secret = ENV["ACCESS_SECRET"]
+end
 markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(autolink: true, hard_wrap: true))
 
 post '/' do
   email = EmailReceiver.receive request
+  subject = EmailReceiver.scrub_fwd(email.subject)
   post = client.post(
-    headline: "Subject: #{EmailReceiver.scrub_fwd(email.subject)}",
+    headline: "Subject: #{subject}",
     body: markdown.render(SimpleScrubber.scrub(email.body.decoded, [:email, :phone])),
     status: "DRAFT"
   )
+  url = post["data"]["permalink"]
   puts post
+  puts url
+  twitter.update "#{subject} #{url}"
   status 200
 end
